3c835487009cc4041571b64676fe839377706b77
Sulabh Shrestha <tsulabh4@gmail.com>
2023-01-25 00:09:15 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,14 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
+// import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import axios from 'axios'
-
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 import '@fontsource/roboto/300.css';
 
@@ -21,6 +25,14 @@ function App() {
   const [message, setMessage] = useState('');
   const [inputmessage, setInputMessage] = useState('');
   const [updated, setUpdated] = useState(message);
+
+  // Radio Value
+  const [radiovalue, setRadioValue] = useState('code');
+
+  const handleRadioChange = (event) => {
+    console.log(event.target.value)
+    setRadioValue(event.target.value);
+  };
 
   const handleChange = (event) => {
     setMessage(event.target.value)
@@ -36,6 +48,7 @@ function App() {
     console.log('Running')
     console.log(message)
     console.log(inputmessage)
+    console.log(radiovalue)
     // axios.get('http://34.224.40.128:5000/')
     // .then(
     //   (response) => {
@@ -99,6 +112,20 @@ function App() {
 
 
 
+
+{/* Radio Here */}
+      
+<RadioGroup
+              aria-labelledby="demo-controlled-radio-buttons-group"
+              name="controlled-radio-buttons-group"
+              value={radiovalue}
+              onChange={handleRadioChange}
+            >
+              <FormControlLabel value="code" control={<Radio />} label="Dockerized Code(Vanilla)" />
+              <FormControlLabel value="image" control={<Radio />} label="Docker Image" />
+            </RadioGroup>
+
+            {/* Radio here */}
 
 
 
